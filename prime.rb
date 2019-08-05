78670d0bc9c6d43e07dd7312cab59dd0de087825
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-05 17:00:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,5 @@
 def prime?(integer)
-if integer<=1
-  return false
-elsif
-  (2..integer).each do |x|
-  (integer % x)==0
-return false
-  end
-else
-  return true
-end
+return false if integer<2
+
+  (2..integer).none?{|x|(integer % x)==0}
 end
